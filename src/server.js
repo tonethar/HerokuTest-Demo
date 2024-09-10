9aaa05dd04a7eda6c98ec40275aecfc5957e3242
tonethar <tonethar@users.noreply.github.com>
2024-09-10 07:59:21 -0400

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(`request.url=${request.url}`);
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.write(`Hello Server - ${new Date().toISOString()}`);
+  response.end();
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Server running at 127.0.0.1:${port}`);
+});
